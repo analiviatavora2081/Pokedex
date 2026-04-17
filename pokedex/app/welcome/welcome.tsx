265b6f76b216a1813c36be942faf319e253087e7
Ana Lívia <analiviatavora2081@gmail.com>
2026-04-17 12:07:23 -0300

    feat: :sparkles: Pokemon

--- pokedex/app/welcome/welcome.tsx
+++ pokedex/app/welcome/welcome.tsx
@@ -1,13 +1,19 @@
 
+
 export function Welcome() {
 
 
 
 
   return (
-    <main>
-      <h1> OI Ana</h1>
-      <button className= "border-2 border bg-red-800 bg-red-700  houver: cursor-poiter bg-red-300"  onClick={() => console.log("oi")}> aperte para falar oi</button>
+    <main  >
+      <p className="text-2xl font-mono text-white dark:text-sky-400 bg-red-700 ">Pokedex
+          <div className="col-start-1 col-end-7 ...">04</div>
+      </p>
 
-
-    </main>)}
+       <h2 className="text-black">Home</h2>
+      <h1>Home</h1>
+      <h1>Home</h1>
+    </main>
+    
+  )}
