79e169d3ebbbad0b15b524a83e34f66760e10937
Ana Lívia <analiviatavora2081@gmail.com>
2026-08-07 10:17:33 -0300

    ..

---
+++ pokedex/app/routes/pokemons.tsx
@@ -0,0 +1,15 @@
+
+import { Pokemon } from "~/pokemon/pokemons";
+import type { Route } from "./+types/home";
+
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Home Page" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <Pokemon />;
+}
